ed3c8dc5d750481c530dbf11d8f999981c720af8
rppradhan08 <rppradhan310@gmail.com>
2020-12-04 11:40:10 +0530

    Modiifications done to Style
    Added base.html for template inheritance

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
 import pickle
-
 from flask import Flask, render_template, request, redirect
 import requests
-
 import numpy as np
 import sklearn
+
+
 app = Flask(__name__)
 
-
+# Loading model
 model = pickle.load(open("Linear_regressor.pkl", "rb"))
 
 
 @app.route('/', methods=['GET'])
 def Home():
     return render_template('index.html')
-
-
+# This function is executed when the form is posted
 @app.route("/predict", methods=['POST'])
 def predict():
     Fuel_Type_Diesel = 0
     if request.method == 'POST':
-
+        # Accessing content of form using there name attribute
         Present_Price = float(request.form['Present_Price'])
         Kms_Driven = int(request.form['Kms_Driven'])
         Owner = int(request.form['Owner'])
@@ -44,16 +43,15 @@ def predict():
             Transmission_Mannual = 0
 
         #model = pickle.load(open(".\\Linear_regressor.pkl", "rb"))
-
         prediction = model.predict([[Present_Price, Kms_Driven, Owner, Age, Fuel_Type_Diesel,
                                      Fuel_Type_Petrol, Seller_Type_Individual, Transmission_Mannual]])
 
         output = round(prediction[0], 2)
 
         if output < 0:
-            return render_template('index.html', prediction_text="Sorry you cannot sell this car")
+            return render_template('result.html', prediction_text="<h2>Sorry you cannot sell this car</h2>")
         else:
-            return render_template('index.html', prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('result.html', prediction_text="You can sell the car at {} Lacs".format(output))
     else:
         return render_template('index.html')
 
